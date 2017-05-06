7d724b3ecfdd9515710e366e341bd8292e78ce1e
minicatsCB <maribeldiaz96@yahoo.es>
2017-05-06 16:05:56 +0200

    Delete user #1 and add email attribute

--- src/main/java/spring/DatabaseLoader.java
+++ src/main/java/spring/DatabaseLoader.java
@@ -16,16 +16,11 @@ public class DatabaseLoader {
 
     @PostConstruct
     private void initDatabase() {
-        // User #1: "user", with password "p1" and role "USER"
-        GrantedAuthority[] userRoles = {
-                new SimpleGrantedAuthority("ROLE_USER") };
-        userRepository.save(new User("user", "p1", Arrays.asList(userRoles)));
-
-        // User #2: "root", with password "p2" and roles "USER" and "ADMIN"
+        // User #1: "root", with password "p2" and roles "USER" and "ADMIN"
         GrantedAuthority[] adminRoles = {
                 new SimpleGrantedAuthority("ROLE_USER"),
                 new SimpleGrantedAuthority("ROLE_ADMIN") };
-        userRepository.save(new User("root", "p2", Arrays.asList(adminRoles)));
-}
+        userRepository.save(new User("root", "p2", "root@root.com",Arrays.asList(adminRoles)));
+    }
 }
 
